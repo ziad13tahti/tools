978b7400736bcf47af3e3a2c8cd9e48a52efe3e8
ziad13tahti <xxziadtahti@gmail.com>
2026-02-09 14:46:55 +0000

    Ajout du script install_tools

---
+++ install_tools.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt install which 
+
+sudo apt install sed
+
+sudo apt install make
+
+sudo apt install binutils
+
+sudo apt install build-essential
+
+sudo apt install diffutils
+
+sudo apt install gcc
+
+sudo apt install g++
+
+sudo apt install bash
+
+sudo apt install patch
+
+sudo apt install gzip
+
+sudo apt install bzip2
+
+sudo apt install perl
+
+sudo apt install tar
+
+sudo apt install cpio
+
+sudo apt install unzip
+
+sudo apt install rsync
+
+sudo apt install file
+
+sudo apt install bc
+
+sudo apt install findutils
+
+sudo apt install awk
+
